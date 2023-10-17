cd1cf2d37d817e6b5e846a87a3acd099e6f94732
Михаил Мигунов <mig.mi@list.ru>
2023-10-17 16:37:02 +0000

    Revert "task2"
    This reverts commit 65dc0f787b6121c72a01cd594043838eaba1a7bd

--- intro/ptr-ref/tasks.h
+++ intro/ptr-ref/tasks.h
@@ -1,13 +1,6 @@
 #pragma once
 
-
-
 namespace NPointers {
-    void Increment(int *x);
-    int Multiply(int first, int second, bool *isOverflow);
-    int ScalarProduct(const int a[], const int b[], int size);
-    int SizeOfMaximumSquareOfCrosses(const char *a, int width, int height);
-    long long int* MultiplyToLongLong(int first, int second);
     /* To implement:
     Increment
     Multiply
@@ -18,8 +11,6 @@ namespace NPointers {
 }
 
 namespace NReferences {
-    void MultiplyInplace(int &a, int b);
-    int CompareArraysByAverage(const int a[], int size, int b[5]);
     /* To implement:
     MultiplyInplace
     CompareArraysByAverage
