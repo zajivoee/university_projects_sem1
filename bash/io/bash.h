e03978b470eb0394585d279f0002acb440662192
zajivoee <mig.mi@list.ru>
2025-02-27 09:58:01 +0300

    stavlu zhopu na to chto zaydet

--- bash/io/bash.h
+++ bash/io/bash.h
@@ -4,53 +4,48 @@
 #include "sys/wait.h"
 #include <stdbool.h>
 
+//void close_file_descriptor(int oldIn, int oldOut, int newIn, int newOut) {
+//    if (newIn != STDIN_FILENO) {
+//        close(newIn);
+//        dup2(newIn, STDIN_FILENO);
+//    }
+//
+//    if (oldOut != STDOUT_FILENO) {
+//        close(newOut);
+//        dup2(newOut, STDOUT_FILENO);
+//    }
+//
+//    close(oldIn);
+//    close(oldOut);
+//}
+
 void Exec(struct Tokenizer* tokenizer) {
     if (tokenizer->token_count == 0) {
         return;
     }
 
-    bool was_in = false, was_out = false;
-    char *in_ptr = NULL, *out_ptr = NULL;
+    bool wasIn = false, wasOut = false;
+    struct Token* cur = tokenizer->head;
 
-    struct Token* cur_token = tokenizer->head;
-
-    size_t words_cnt = 0;
-
-    while (cur_token) {
-        if (cur_token->type == TT_INFILE) {
-            if (was_in) {
-                printf("Syntax error\n");
-                return;
-            }
-            
-            was_in = true;
-            
-            if (cur_token->next == NULL || cur_token->next->type != TT_WORD) {
+    while (cur) {
+        if (cur->type == TT_INFILE) {
+            if (!cur->next || wasIn || cur->next->type != TT_WORD) {
                 printf("Syntax error\n");
                 return;
             }
 
-            in_ptr = cur_token->next->start;
-            cur_token = cur_token->next;
-        } else if (cur_token->type == TT_OUTFILE) {
-            if (was_out) {
-                printf("Syntax error\n");
-                return;
-            }
-            
-            was_out = true;
-            
-            if (cur_token->next == NULL || cur_token->next->type != TT_WORD) {
+            wasIn = true;
+            cur = cur->next;
+        } else if (cur->type == TT_OUTFILE) {
+            if (!cur->next || wasOut || cur->next->type != TT_WORD) {
                 printf("Syntax error\n");
                 return;
             }
 
-            out_ptr = cur_token->next->start;
-            cur_token = cur_token->next;
-        } else {
-            words_cnt++;
-            cur_token = cur_token->next;
+            wasOut = true;
+            cur = cur->next;
         }
+        cur = cur->next;
     }
 
     char** ARGV = malloc(sizeof(char *) * (tokenizer->token_count + 1));
@@ -59,67 +54,67 @@ void Exec(struct Tokenizer* tokenizer) {
         return;
     }
 
-    size_t token_num = 0;
-    cur_token = tokenizer->head;
+    cur = tokenizer->head;
+    size_t wordsCnt = 0;
 
-    while (cur_token) {
-        if (cur_token->type == TT_INFILE || cur_token->type == TT_OUTFILE) {
-            cur_token = cur_token->next;
+    int in_fd = STDIN_FILENO, out_fd = STDOUT_FILENO;
 
-        } else {
-            cur_token->start[cur_token->len] = '\0';
-            ARGV[token_num++] = cur_token->start;
-            cur_token = cur_token->next;
+    while (cur) {
+        if (cur->type == TT_WORD) {
+            cur->start[cur->len] = '\0';
+            ARGV[wordsCnt++] = cur->start;
+        } else if (cur->type == TT_INFILE) {
+            cur = cur->next;
+
+            cur->start[cur->len] = '\0';
+            in_fd = open(cur->start, O_RDONLY);
+
+            if (in_fd < 0) {
+                printf("I/O error\n");
+                free(ARGV);
+                return;
+            }
+        } else if (cur->type == TT_OUTFILE) {
+            cur = cur->next;
+
+            cur->start[cur->len] = '\0';
+
+            out_fd = open(cur->start, O_WRONLY | O_CREAT | O_TRUNC);
+            if (out_fd < 0) {
+                printf("I/O error\n");
+                close(out_fd);
+                free(ARGV);
+                return;
+            }
         }
+        cur = cur->next;
     }
 
-    if (ARGV[0] == NULL) {
-        printf("Syntax Error\n");
-        return;
-    }
-
-    ARGV[token_num] = NULL;
-
-    int in = -1, out = -1;
-    if (in_ptr != NULL) {
-        in = open(in_ptr, O_RDONLY);
-        if (in < 0) {
-            printf("I/O error\n");
-            free(ARGV);
-            return;
-        }
-    }
-
-    if (out_ptr != NULL) {
-        out = open(out_ptr, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (out < 0) {
-            printf("I/O error\n");
-            free(ARGV);
-            return;
-        }
-    }
+    ARGV[wordsCnt] = NULL;
 
     pid_t pid = fork();
     if (pid < 0) {
         free(ARGV);
         return;
     } else if (pid == 0) {
-        if (in != -1) {
-            dup2(in, STDIN_FILENO);
-            close(in);
+        if (in_fd != STDIN_FILENO) {
+            dup2(in_fd, STDIN_FILENO);
+            close(in_fd);
         }
 
-        if (out != -1) {
-            dup2(out, STDOUT_FILENO);
-            close(out);
+        if (out_fd != STDOUT_FILENO) {
+            dup2(out_fd, STDOUT_FILENO);
+            close(out_fd);
         }
 
         execvp(ARGV[0], ARGV);
-
         printf("Command not found\n");
-        fflush(stdout);
-        _exit(1);
+        return;
     } else {
+        if (in_fd != STDIN_FILENO)
+            close(in_fd);
+        if (out_fd != STDOUT_FILENO)
+            close(out_fd);
         wait(NULL);
     }
 
