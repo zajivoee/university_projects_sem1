92abe52c1ba83ae5366b6d63051325ff10dbaa0f
zajivoee <mig.mi@list.ru>
2025-03-09 23:10:50 +0300

    restore2 + changes

--- asm/syscall/syscall.c
+++ asm/syscall/syscall.c
@@ -1,40 +1,167 @@
 #include "syscall.h"
 
 int open(const char* pathname, int flags) {
-    // your code here
-}
-int close(int fd) {
-    // your code here
+    int fd;
+
+    asm volatile (
+            "mov $2, %%rax\n"
+            "mov %1, %%rdi\n"
+            "mov %2, %%rsi\n"
+            "syscall\n"
+            "mov %%eax, %0"
+            : "=rm" (fd)
+            : "rm" (pathname), "rm" (flags)
+            : "rax", "rdi", "rsi", "rcx"
+            );
+
+    return fd;
 }
 
 ssize_t read(int fd, void* buf, size_t count) {
-    // your code here
+    ssize_t count_bytes;
+
+    asm volatile (
+            "mov $0, %%eax\n"
+            "mov %1, %%edi\n"
+            "mov %2, %%rsi\n"
+            "mov %3, %%rdx\n"
+            "syscall\n"
+            "mov %%rax, %0"
+            : "=rm" (count_bytes)
+            : "rm" (fd), "rm" (buf), "rm" (count)
+            : "eax", "edi", "rsi", "rdx"
+            );
+
+    return count_bytes;
 }
 
 ssize_t write(int fd, const void* buf, ssize_t count) {
-    // your code here
+    ssize_t count_bytes;
+
+    asm volatile (
+            "mov $1, %%eax\n"
+            "mov %1, %%rdi\n"
+            "mov %2, %%rsi\n"
+            "mov %3, %%rdx\n"
+            "syscall\n"
+            "mov %%rax, %0"
+            : "=rm" (count_bytes)
+            : "rm" (fd), "rm" (buf), "rm" (count)
+            : "eax", "rdi", "rsi"
+            );
+
+    return count_bytes;
 }
 
 int pipe(int pipefd[2]) {
-    // your code here
+    int res;
+
+    asm volatile (
+            "mov $22, %%eax\n"
+            "mov %1, %%rdi\n"
+            "syscall\n"
+            "mov %%eax, %0"
+            : "=rm" (res)
+            : "rm" (pipefd)
+            : "eax", "rdi", "rcx"
+            );
+
+    return res;
+}
+
+int close(int fd) {
+    int res;
+
+    asm volatile (
+            "mov $3, %%eax\n"
+            "mov %1, %%rdi\n"
+            "syscall\n"
+            "mov %%eax, %0"
+            : "=rm" (res)
+            : "rm" (fd)
+            : "eax", "rdi", "rcx"
+            )
+            ;
+    return res;
 }
 
 int dup(int oldfd) {
-    // your code here
-}
+    int newfd;
 
-pid_t fork() {
-    // your code here
-}
+    asm volatile (
+            "mov $32, %%eax\n"
+            "mov %1, %%rdi\n"
+            "syscall\n"
+            "mov %%eax, %0"
+            : "=rm" (newfd)
+            : "rm" (oldfd)
+            : "eax", "rdi", "rcx"
+            );
 
-pid_t waitpid(pid_t pid, int* wstatus, int options) {
-    // your code here
-}
-
-int execve(const char* filename, char* const argv[], char* const envp[]) {
-    // your code here
+    return newfd;
 }
 
 void exit(int status) {
-    // your code here
+    asm volatile (
+            "mov $60, %%eax\n"
+            "mov %0, %%rdi\n"
+            "syscall\n"
+            :
+            : "rm" (status)
+            : "eax", "rdi", "rcx", "r11"
+            );
+
+    __builtin_unreachable();
+}
+
+pid_t fork() {
+    pid_t pid;
+
+    asm volatile (
+            "mov $57, %%eax\n"
+            "syscall\n"
+            "mov %%eax, %0"
+            : "=rm" (pid)
+            :
+            : "eax", "rcx", "r11"
+            );
+
+    return pid;
+}
+
+pid_t waitpid(pid_t pid, int* wstatus, int options) {
+    pid_t res;
+
+    asm volatile (
+            "mov $61, %%eax\n"
+            "mov %1, %%rdi\n"
+            "mov %2, %%rsi\n"
+            "mov %3, %%rdx\n"
+            "syscall\n"
+            "mov %%eax, %0"
+            : "=rm" (res)
+            : "rm" (pid), "rm" (wstatus), "rm" (options)
+            : "eax", "rdi", "rsi", "rdx"
+            );
+    
+    return res;
+}
+
+
+int execve(const char* filename, char* const argv[], char* const envp[]) {
+    int res;
+
+    asm volatile (
+            "mov $59, %%eax\n"
+            "mov %1, %%rdi\n"
+            "mov %2, %%rsi\n"
+            "mov %3, %%rdx\n"
+            "syscall\n"
+            "mov %%eax, %0"
+            : "=rm" (res)
+            : "rm" (filename), "rm" (argv), "rm" (envp)
+            : "eax", "rdi", "rsi", "rdx"
+            );
+    
+    return res;
 }
